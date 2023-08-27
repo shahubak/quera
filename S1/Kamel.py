a5b5ecea6f56c513cf9172d0a9d47528b4c0784e
shahubak <shahab.bozorgi007@gmail.com>
2023-08-28 00:09:25 +0330

    change kamel

--- S1/Kamel.py
+++ S1/Kamel.py
@@ -1,14 +1,12 @@
-
- 
 n=int(input())
-
 mylist = []
 for i in range(1,n):
     if(n%i==0):
         mylist.append(i)
 
-
+print("well")
 if sum(mylist) == n:
     print("Yes")
 else:
     print("No")
+
